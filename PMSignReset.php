ffcca69fc05c1dacd68b328b3ed40f5e0f115360
A9-0Z <samuelandrewmark@gmail.com>
2013-08-09 01:02:13 +0100

    Update PMSignReset.php

--- PMSignReset.php
+++ PMSignReset.php
@@ -25,7 +25,7 @@ class PMSignReset implements Plugin{
         $this->api->addHandler("player.block.touch", array($this, "touchHandler"));
   	$this->path = $this->api->plugin->configPath($this);
 		$this->config = new Config($this->path."config.yml", CONFIG_YAML, array(
-			'BlockId' => '255',
+			'BlockId' => '248',
 			'MsgWhenGiven' => 'Signs reset !',
 		));
 
