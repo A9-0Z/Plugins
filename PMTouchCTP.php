97a6c2045dab8eebca2e2fee413ad4046cfdb4c4
A9-0Z <samuelandrewmark@gmail.com>
2013-08-19 18:01:26 +0100

    Update PMTouchCTP.php

--- PMTouchCTP.php
+++ PMTouchCTP.php
@@ -17,22 +17,7 @@ Small Changelog
 */
 
 class PMTouchTP implements Plugin{
-
-  private $api, $lang, $groupmanager;
-private static $cmds = array(
-"lobby",
-	);
-
-public function __construct(ServerAPI $api, $server = false){
-$this->api = $api;
-$this->groupmanager = false;
-}
-
-    private $api;
-    public function __construct(ServerAPI $api, $server = false){
-        $this->api = $api;
-    }
-    public function init(){
+   public function init(){
         $this->api->addHandler("player.block.touch", array($this, "touchHandler"));
 $this->path = $this->api->plugin->configPath($this);
 $this->api->console->register("lobby", "", array($this, "defaultCommands"));
@@ -59,31 +44,8 @@ $this->api->chat->sendTo(false, $this->config->get('MsgWhenGiven'), $username);
     }
     
 
-$this->block = (int)$this->config->get('BlockId2'); }
 
-    public function touchHandler($data){
-        $target = $data["target"];
-        if ($target->getID() === $this->block){
-$username = $data["player"]->username;
-$player = $this->api->player->get($username);
-$this->api->console->run("tp $player w:Nether");
-$this->api->chat->sendTo(false, $this->config->get('MsgWhenGiven2'), $username);
-}
 
-        
-    }
-    
-    	public function defaultCommands($cmd, $params, $issuer, $alias){
-$output = "";
-switch($cmd){
-case "lobby":
-if(!($issuer instanceof Player)){
-$output .= "Please run this command in-game.\n";
-break;
-}
-if($this->data[$issuer->iusername]->exists("lobby")){
-$this->api->console->run("tp $issuer w:world");
-}
     public function __destruct(){
 
     }
