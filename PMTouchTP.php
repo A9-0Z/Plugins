351045dd277e64837351125e31b1054f8a5bb24e
A9-0Z <samuelandrewmark@gmail.com>
2013-08-20 18:19:30 +0100

    Update PMTouchTP.php

--- PMTouchTP.php
+++ PMTouchTP.php
@@ -25,11 +25,14 @@ class PMTouchTP implements Plugin{
         $this->api->addHandler("player.block.touch", array($this, "touchHandler"));
 		  $this->path = $this->api->plugin->configPath($this);
 		  $this->config = new Config($this->path."config.yml", CONFIG_YAML, array(
-'BlockId' => '155', '246',
+'BlockId' => '155',
+'BlockId2' => '246',
 'MsgWhenGiven' => 'Welcome to Skyblock Arena !',
-'MsgWhenGiven2' => 'Welcome to Nether Arena !',)
+'MsgWhenGiven2' => 'Welcome to Nether Arena !',
+'MsgWhenGiven3' => 'Please stand still until world loads !',)
 );
-$this->block = (int)$this->config->get('BlockId'); }
+$this->block = (int)$this->config->get('BlockId');
+$this->block2 = (int)$this->config->get('BlockId2');}
 
     public function touchHandler($data){
         $target = $data["target"];
@@ -38,13 +41,13 @@ $this->block = (int)$this->config->get('BlockId'); }
 $username = $data["player"]->username;
 $player = $this->api->player->get($username);
 $this->api->console->run("tp $player w:Skyblock");
-$this->api->chat->sendTo(false, $this->config->get('MsgWhenGiven'), $username);
+$this->api->chat->sendTo(false, $this->config->get('MsgWhenGiven', 'MsgWhenGiven3'), $username);
 }
-        if ($target->getID() === $this->block){
+        if ($target->getID() === $this->block2){
 $username = $data["player"]->username;
 $player = $this->api->player->get($username);
 $this->api->console->run("tp $player w:Nether");
-$this->api->chat->sendTo(false, $this->config->get('MsgWhenGiven2'), $username);
+$this->api->chat->sendTo(false, $this->config->get('MsgWhenGiven2', 'MsgWhenGiven3'), $username);
 }
         
     }
