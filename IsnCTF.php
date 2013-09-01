6809e521a6fc06e0eab5c844d3bc27aa0d9da585
A9-0Z <samuelandrewmark@gmail.com>
2013-09-01 15:45:46 +0100

    Update IsnCTF.php

--- IsnCTF.php
+++ IsnCTF.php
@@ -129,7 +129,7 @@ class IsnCTF implements Plugin{
       		$x = $target->entity->x;
                 $y = $target->entity->y;
                 $z = $target->entity->z;
-                if($x === 66 and $y === 65 and $z === 64){
+                if(65<= $x)&&($x <= 67 and 64<= $y)&&($y <= 66 and 63<= $z)&&($z <= 65){
                         $this->api->chat->broadcast("[ISN] " . 'Red Team Scored !');	
                 	$this->api->chat->broadcast("[ISN] " . 'Flag Captured by $username !');
                 	 array_push($GLOBALS['RedSC'],$username);
